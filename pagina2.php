31ecb814cb6a8540b1214257f9d792272930afe0
ZentEdwin <ezenteno@elearning.com.gt>
2018-03-01 10:23:47 -0600

    camb

--- pagina2.php
+++ pagina2.php
@@ -3,7 +3,6 @@
 	$asunto = $_POST['asunto'];
 	$mensaje = $_POST['mensaje'];
 
-	echo $nombre. "ha dicho <br/>".$mensaje;
 	if(mail('ezenteno@elearning.com.gt', $asunto, $mensaje)){
 		echo "mail enviado";
 	}else{
